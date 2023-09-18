0a57ddf76b187b0f18ca3dc1101917ab33aec468
MiguelArc0 <91166753+MiguelArc0@users.noreply.github.com>
2023-09-18 20:21:11 -0300

    Update Teams.java
    Tarefa Adriana Dia 18 / 09

--- Teste/src/Teams.java
+++ Teste/src/Teams.java
@@ -87,6 +87,135 @@ public class Teams {
 
     }
 
+        static void ex81ArraySimples(){
+        int[] notas = new int[5];
+        Scanner sc = new Scanner(System.in);
+        double arma_media = 0;
+        double media = 0;
+        for(int i = 0; i < notas.length; i ++ ){
+            System.out.println("Insira os números: ");
+            notas[i] = sc.nextInt();
+            arma_media += notas[i];
+        }
+        media = arma_media / notas.length;
+        int menor = notas[0];
+        int pos = 1;
+        for(int i = 0; i < notas.length; i ++ ){
+            if(menor > notas[i]){
+                menor = notas[i];
+                pos = (i+1);
+            }
+            else{
+                pos = (i+1);
+            }
+        }
+        System.out.println("O Array: " + Arrays.toString(notas));
+        System.out.println("A média das notas: " + media);
+        System.out.println("A menor das notas: " + menor);
+        System.out.println("A posição do Maior é o número " + pos +" na lista");
+
+
+    }
+        
+        static void ex82(){
+
+        int[] notasV = new int[5];
+        int[] notasM = new int[5];
+        Scanner sc = new Scanner(System.in);
+
+        for(int i = 0; i < notasV.length; i ++ ) {
+            System.out.println("Insira os números: ");
+            notasV[i] = sc.nextInt();
+        }
+        System.out.println("Insira mais um número: ");
+        int maisum = sc.nextInt();
+
+        for(int i = 0; i < notasM.length; i++){
+            notasM[i] = notasV[i] * maisum;
+        }
+        System.out.println("O Array Original: " + Arrays.toString(notasV));
+        System.out.println("O Array Multiplicado: " + Arrays.toString(notasM));
+
+    }
+    
+
+        static void ex83ArraySimples(){
+        int[] notas = new int[5];
+        Scanner sc = new Scanner(System.in);
+        for(int i = 0; i < notas.length; i ++ ) {
+            System.out.println("Insira os números: ");
+            notas[i] = sc.nextInt();
+        }
+        for(int i = 1; i < notas.length; i ++ ) {
+            System.out.println("Da primeira até a ultima: " + i);
+        }
+
+        System.out.println("----------------------");
+
+        for(int i = 1; i < notas.length; i ++ ) {
+            System.out.println("Da última até o começo: " + notas[notas.length - i]);
+        }
+        System.out.println("Da última até o começo: " + notas[0]);
+        System.out.println("O Array: " + Arrays.toString(notas));
+
+    }
+
+    
+
+        static void ex84() {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = 0;
+        System.out.println("Insira o valor de N: ");
+        n = sc.nextInt();
+
+        int[] Aba = new int[n];
+        int[] Bab = new int[n];
+        int[] Soma = new int[n];
+
+        for (int i = 0; i < Aba.length; i++) {
+            System.out.println("Insira os valores do vetor A: ");
+            Aba[i] = sc.nextInt();
+
+            System.out.println("Insira os valores do vetor B: ");
+            Bab[i] = sc.nextInt();
+        }
+//System.out.println("Insira os valores do vetor B: ");
+//            Aba[i] = sc.nextInt();
+//
+        for (int i = 0; i < Aba.length; i++) {
+            Soma[i] = Aba[i] + Bab[i];
+        }
+
+        System.out.println("O vetor A: " + Arrays.toString(Aba));
+        System.out.println("O vetor B: " + Arrays.toString(Bab));
+        System.out.println("O vetor das somas de A e B: " + Arrays.toString(Soma));
+
+
+    }
+
+        static void ex85() {
+        Scanner sc = new Scanner(System.in);
+
+        int[] temp = new int[5];
+
+        for (int i = 0; i < temp.length; i++) {
+            System.out.println("Insira as temperaturas: ");
+            temp[i] = sc.nextInt();
+
+        }
+        int maior_temp = temp[0];
+        for(int i = 0; i < temp.length; i++){
+            if (maior_temp < temp[i]){
+                maior_temp = temp[i];
+            }
+        }
+        System.out.println("Maior valor: " + maior_temp);
+
+
+    }
+
     
     static void ex86(){
         Scanner sc = new Scanner(System.in);
@@ -134,6 +263,52 @@ static void ex87(){
 
 //        System.out.println("Insira mais um valor: ");
 //        int mais = sc.nextInt();
+
+
+    }
+
+        static void ex88ArraySimples(){
+        int[] notas = new int[5];
+        Scanner sc = new Scanner(System.in);
+        for(int i = 0; i < notas.length; i ++ ) {
+            System.out.println("Insira os números: ");
+            notas[i] = sc.nextInt();
+        }
+        boolean existe = false;
+        System.out.println("Insira mais um número: ");
+        int maisum = sc.nextInt();
+        for(int i = 0; i < notas.length; i ++ ) {
+            if (maisum == notas[i]){
+                existe = true;
+            }
+        }
+
+    
+
+    static void ex89(){
+        int[] V1 = new int[5];
+        int[] V2 = new int[5];
+
+        int qtd_Vezes = 0;
+        int msm_pos = 0;
+        Scanner sc = new Scanner(System.in);
+        for(int i = 0; i < V1.length; i ++ ) {
+            System.out.println("Insira os números: ");
+            V1[i] = sc.nextInt();
+        }
+        for(int i = 0; i < V1.length; i ++ ) {
+            System.out.println("Insira os números VETOR 2: ");
+            V2[i] = sc.nextInt();
+        }
+        for(int i = 0; i < V1.length; i ++ ) {
+            if(V1[i] == V2[i]){
+                qtd_Vezes += 1;
+            }
+
+        }
+        System.out.println(Arrays.toString(V1));
+        System.out.println(Arrays.toString(V2));
+        System.out.println("Qtd vezes iguais em que é o mesmo número e na mesma posição: " + qtd_Vezes);
 
 
     }
