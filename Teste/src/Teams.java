d367221d1b643388ec925f9c3a2c3673b6d7c96e
MiguelArc0 <91166753+MiguelArc0@users.noreply.github.com>
2023-10-09 20:10:48 -0300

    Update Teams.java

--- Teste/src/Teams.java
+++ Teste/src/Teams.java
@@ -800,6 +800,66 @@ static void ex87ArrayList(){
         }
     }
 }
+
+
+package entities;
+
+public class Conta {
+    private Integer number;
+    private String holder;
+    private Double balance;
+    private Double limitesaque;
+
+    public Conta(){
+
+    }
+    public Conta(Integer number, String holder,Double balance, Double limitesaque ){
+        this.number = number;
+        this.holder = holder;
+        this.balance = balance;
+        this.limitesaque = limitesaque;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public String getHolder() {
+        return holder;
+    }
+
+    public void setHolder(String holder) {
+        this.holder = holder;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
+
+    public Double getLimitesaque() {
+        return limitesaque;
+    }
+
+    public void setLimitesaque(Double limitesaque) {
+        this.limitesaque = limitesaque;
+    }
+    public void depositar(double montante){
+            balance = balance + montante;
+    }
+    public void sacar(double montante){
+        balance = balance - montante;
+    }
+
+}
+
             
 
 }
